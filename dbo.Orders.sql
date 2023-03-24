cd392d7c0f767af2f31c19d604540ee20a77b9cd
hadiayachee <99985867+hadiayachee@users.noreply.github.com>
2023-03-24 15:20:41 +0200

    Add files via upload

---
+++ dbo.Orders.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [ProdName] VARCHAR(50) NOT NULL,
+    [Price]    INT            NOT NULL,
+    [Qty]      INT            NOT NULL,
+    [Date]     VARCHAR (50)   NOT NULL,
+    [Total]    INT            NOT NULL
+);
+
